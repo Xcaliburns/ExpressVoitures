35c49f8b014775ba74fe359c815360baa206d367
Xcaliburns <davidabruzzogames@gmail.com>
2024-08-08 12:44:11 +0200

    unit tests project added

---
+++ DotnetProjet5.UnitTests/Controllers/VehiclesControllerTests.cs
@@ -0,0 +1,67 @@
+﻿using Xunit;
+using DotnetProjet5.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotnetProjet5.Controllers.Tests
+{
+    public class VehiclesControllerTests
+    {
+        [Fact()]
+        public void VehiclesControllerTest()
+        {
+            Xunit.Assert.Fail("This test needs an implementation");
+        }
+
+        [Fact()]
+        public void IndexTest()
+        {
+            Xunit.Assert.Fail("This test needs an implementation");
+        }
+
+        [Fact()]
+        public void DetailsTest()
+        {
+            Xunit.Assert.Fail("This test needs an implementation");
+        }
+
+        [Fact()]
+        public void CreateTest()
+        {
+            Xunit.Assert.Fail("This test needs an implementation");
+        }
+
+        [Fact()]
+        public void CreateTest1()
+        {
+            Xunit.Assert.Fail("This test needs an implementation");
+        }
+
+        [Fact()]
+        public void EditTest()
+        {
+            Xunit.Assert.Fail("This test needs an implementation");
+        }
+
+        [Fact()]
+        public void EditTest1()
+        {
+            Xunit.Assert.Fail("This test needs an implementation");
+        }
+
+        [Fact()]
+        public void DeleteTest()
+        {
+            Xunit.Assert.Fail("This test needs an implementation");
+        }
+
+        [Fact()]
+        public void DeleteConfirmedTest()
+        {
+            Xunit.Assert.Fail("This test needs an implementation");
+        }
+    }
+}
