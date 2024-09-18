ef41210652081cd56d7370ef2d9b406ed7b02dbe
Xcaliburns <davidabruzzogames@gmail.com>
2024-09-18 09:37:40 +0200

    repairs fix

--- DotnetProjet5002/Controllers/RepairsController.cs
+++ DotnetProjet5002/Controllers/RepairsController.cs
@@ -225,7 +225,7 @@ namespace DotnetProjet5.Controllers
         // POST: Repairs/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        
+
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var repair = await _context.Repairs.FindAsync(id);
